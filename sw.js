440357fec48d10dd89ada42d19c9730e2d944a13
ssandorov <49904591+SSandorov@users.noreply.github.com>
2022-06-29 23:14:15 +0200

    Navegación offline

---
+++ sw.js
@@ -0,0 +1,83 @@
+/*
+En este ejemplo vamos a emplear la segunda estrategia del caché
+
+Cache with network fallback
+*/
+
+// Empleamos las variables y la función para gestionar la memoria y la instalación del SW
+
+const CACHE_STATIC_NAME  = 'static-v1';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
+const CACHE_INMUTABLE_NAME = 'inmutable-v1';
+
+const CACHE_DYNAMIC_LIMIT = 50;
+
+function limpiarCache( cacheName, numeroItems ) {
+
+    caches.open( cacheName )
+        .then( cache => {
+
+            return cache.keys()
+                .then( keys => {
+                    
+                    if ( keys.length > numeroItems ) {
+                        cache.delete( keys[0] )
+                            .then( limpiarCache(cacheName, numeroItems) );
+                    }
+                });
+            
+        });
+}
+
+self.addEventListener('install', e => {
+
+    const cacheProm = caches.open( CACHE_STATIC_NAME )
+        .then( cache => {
+
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/css/style.css',
+                '/img/main.jpg',
+                '/js/app.js',
+                '/img/no-img.jpg',
+                '/pages/offline.html'
+            ]);
+        
+        });
+
+    const cacheInmutable = caches.open( CACHE_INMUTABLE_NAME )
+            .then( cache => cache.add('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'));
+
+    e.waitUntil( Promise.all([cacheProm, cacheInmutable]) );
+
+});
+
+self.addEventListener('fetch', e => {
+    const respuesta = caches.match( e.request )
+        .then( res => {
+            // existe en caché la respuesta
+            if ( res ) return res;
+
+            // No existe el archivo
+            // tengo que ir a la web
+            return fetch( e.request ).then( newResp => {
+
+                caches.open( CACHE_DYNAMIC_NAME )
+                    .then( cache => {
+                        cache.put( e.request, newResp );
+                        limpiarCache( CACHE_DYNAMIC_NAME, 50 );
+                    });
+
+                return newResp.clone();
+            });
+
+
+        });
+
+
+
+
+    e.respondWith( respuesta );
+
+})
